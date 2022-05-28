d8de74a8ac6546fa81803d9f5e44328c2359f055
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-28 21:44:09 +0000

    Movie model

--- watchlist/movie/admin.py
+++ watchlist/movie/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Movie
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ["title", "release_date", "active"]
+    list_filter = ["release_date", "active"]
+    search_fields = ["title", "relase_date"]
+    ordering = ["-release_date"]
--- watchlist/movie/models.py
+++ watchlist/movie/models.py
@@ -1,1 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Movie(models.Model):
+    title = models.CharField(_("Title"), max_length=100)
+    description = models.TextField(_("Description"), blank=True)
+    release_date = models.DateField(_("Release date"), blank=True, null=True)
+    active = models.BooleanField(_("Active"), default=True)
+    created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
