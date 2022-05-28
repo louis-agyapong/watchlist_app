15c1c54196f273272e18da1db0b71916f444b6a6
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-28 21:46:29 +0000

    changed allowed host settings

--- watchlist/settings.py
+++ watchlist/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=True, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
+ALLOWED_HOSTS = config("LOCAL_HOSTS", cast=Csv())
 
 
 # Application definition
