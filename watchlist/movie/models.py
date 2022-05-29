3a6a3f6454bc087673070fabc80a0ef1fa6247fe
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-29 10:59:04 +0000

    model validation

--- watchlist/movie/models.py
+++ watchlist/movie/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
 
 class Movie(models.Model):
@@ -9,6 +10,13 @@ class Movie(models.Model):
     active = models.BooleanField(_("Active"), default=True)
     created_at = models.DateTimeField(_("Created at"), auto_now_add=True)
     updated_at = models.DateTimeField(_("Updated at"), auto_now=True)
+
+    def clean(self) -> None:
+        if len(self.title) < 4:
+            raise ValidationError(
+                _("Title must be at least 3 characters long."),
+                code="title_too_short",
+            )
 
     def __str__(self) -> str:
         return self.title
