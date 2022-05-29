f8834fa0a0cc0ea6bfcc0135dcc45ba98413fa83
Louis Agyapong <agyapong.louis@gmail.com>
2022-05-29 10:58:44 +0000

    model level validation

--- watchlist/movie/api/serializers.py
+++ watchlist/movie/api/serializers.py
@@ -9,13 +9,13 @@ class MovieSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Movie
-        fields = (
-            "id",
-            "title",
-            "description",
-            "release_date",
-            "active",
-            "created_at",
-            "updated_at",
-        )
+        fields = ("id", "title", "description", "release_date", "active", "created_at", "updated_at")
         read_only_fields = ("created_at", "updated_at", "id")
+
+    def validate_title(self, value):
+        """
+        Check if the title is at least 3 characters long.
+        """
+        if len(value) < 3:
+            raise serializers.ValidationError("Title must be at least 3 characters long.")
+        return value
