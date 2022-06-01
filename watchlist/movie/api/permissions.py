1a69cfb3f787528b510a496f39cc6396df69f35f
Louis Agyapong <agyapong.louis@gmail.com>
2022-06-01 18:15:31 +0000

    review user or read only permission

--- watchlist/movie/api/permissions.py
+++ watchlist/movie/api/permissions.py
@@ -14,3 +14,14 @@ class AdminOrReadOnly(permissions.IsAdminUser):
     def has_permission(self, request, view):
         admin_permission = bool(request.user and request.user.is_staff)
         return request.method == "GET" or admin_permission
+
+
+class ReviewUserOrReadOnly(permissions.BasePermission):
+    """Allow users to edit their own reviews, non-users to read"""
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            # Read permissions are allowed to any request,
+            return True
+
+        return obj.review_user == request.user
