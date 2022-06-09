4ec78d5949d1ec3789146ee07b8a54557c2c1317
Louis Agyapong <agyapong.louis@gmail.com>
2022-06-09 21:59:15 +0000

    changed account token endpoint to account/login/ endpoint

--- watchlist/user_app/api/urls.py
+++ watchlist/user_app/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path("token/", obtain_auth_token, name="api-token"),
+    path("login/", obtain_auth_token, name="login"),
 ]
